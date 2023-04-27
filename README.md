c7c81c2ac296d0a329d47e7b34951153f9382177
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2023-04-26 17:09:19 -0700

    Working now, will update and add later

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 Logical operators are often a difficult concept for students at the 102 level to grasp. Especially since we don't force the use of logical operators in any of the assignments in the Code 102 curriculum. So it's easy to overlook and/or avoid utilizing these concepts...for now!
 
 The goal of this project is to provide a resource to test a student's comprehension of logical operators and evaluating expressions.
+
+## Practice Functions
+
+1. Write a function isEvenAndDivisibleByFour that takes a number as an argument and returns true if the number is even and divisible by 4, and false otherwise.
+2. Write a function isVowelOrEven that takes a string as an argument and returns true if the string contains a vowel or its length is even, and false otherwise.
+3. Write a function isBetween that takes three numbers (a, b, c) as arguments and returns true if b is between a and c, and false otherwise.
+4. Write a function hasEnoughCoins that takes two numbers (quarters, dimes) as arguments and returns true if the total value of the coins is at least 75 cents, and false otherwise.
+5. Write a function isPalindrome that takes a string as an argument and returns true if the string is the same forwards and backwards, and false otherwise.
+6. Write a function isMultipleOfThreeOrFive that takes a number as an argument and returns true if the number is a multiple of 3 or 5, and false otherwise.
+7. Write a function isLeapYear that takes a year as an argument and returns true if the year is a leap year (divisible by 4, but not by 100 unless also divisible by 400), and false otherwise.
+8. Write a function hasUniqueCharacters that takes a string as an argument and returns true if all the characters in the string are unique, and false otherwise.
--- app.js
+++ app.js
@@ -1,2 +1,67 @@
 'use strict';
 
+// array of expressions
+let expressions = [
+  '5 > 3 && 10 < 20', 
+  '3 < 2 || 5 > 10',
+  '!(4 == 4)',
+  '7 >= 6 && !(8 != 8)',
+  '5 * 2 == 10 && "hello" != "world"',
+  'true && false || true',
+  '!(10 < 5) || (8 > 6 && 4 <= 4)',
+  '(3 < 5 && 4 > 2) || (1 == 1 && false)'
+]
+
+// array of correct evaluations
+let answers = [
+  'true',
+  'false',
+  'false',
+  'true',
+  'true',
+  'true',
+  'true',
+  'true'
+]
+
+let explanations = [
+  'This expression will evaluate to true because both conditions are true.',
+  'This expression will evaluate to false because both conditions are false.', 
+  'This expression will evaluate to false because the condition inside the parentheses is true, but the ! operator negates it.',
+  'This expression will evaluate to true because the first condition is true and the second condition is false (negated by the ! operator).',
+  'This expression will evaluate to true because both conditions are true.',
+  'This expression will evaluate to true because the first two conditions (true && false) evaluate to false, but the third condition (true) is true.',
+  'This expression will evaluate to true because the first condition (!(10 < 5)) is true (negated by the ! operator), and the second condition (8 > 6 && 4 <= 4) is also true.',
+  'This expression will evaluate to true because the first condition (3 < 5 && 4 > 2) is true, and the second condition (1 == 1 && false) is false, but the || operator evaluates to true if at least one condition is true.'
+]
+
+function renderExpressions(){
+  for (let i = 0; i < expressions.length; i++){
+    document.write(`
+      <li class='expressions'>
+        ${expressions[i]} 
+        <section class='buttons'>
+          <button id='${i}' onClick=solveIt(${i})>Solve me!</button>
+          <button onClick=renderExplanation(${i})>See Answer</button>
+        </section>
+      </li>`
+    );
+  }
+}
+
+function solveIt(index){
+  let userAnswer = prompt(`True or False: ${expressions[index]}`);
+  if (userAnswer == answers[index]){
+    alert(`You are correct! It is ${answers[index]}`);
+    renderExplanation(index);
+  } else {
+    if (confirm('That is incorrect. Try again?')){
+      solveIt(index);
+    };
+  }
+}
+
+function renderExplanation(index){
+  alert(`${expressions[index]} : ${explanations[index]}`);
+}
+
